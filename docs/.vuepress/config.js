50ecd51faacd61e903ccf4efcc7639e8e6b4dea9
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2023-01-22 14:55:00 +1100

    Testing some mroe.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,62 +1,90 @@
-import { defineUserConfig } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
-import { defaultTheme } from '@vuepress/theme-default'
-import { gitPlugin } from '@vuepress/plugin-git'
+import {  defaultTheme } from '@vuepress/theme-default'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { searchPlugin } from '@vuepress/plugin-search'
 
-export default defineUserConfig({
+export default {
   title: 'Connor Forsyth',
   description: 'A place to share knowledge.',
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "favicon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "favicon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "favicon.png"}],
-    ['link', { rel: "manifest", href: "/"}],
-    ['link', { rel: "mask-icon", href: "favicon.png", color: "#3a0839"}],
-    ['link', { rel: "shortcut icon", href: "favicon.png"}],
-    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
-    ['meta', { name: "msapplication-config", content: "favicon.png"}],
-    ['meta', { name: "theme-color", content: "#ffffff"}],
+    ['link', {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "favicon.png"
+    }],
+    ['link', {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "favicon.png"
+    }],
+    ['link', {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "favicon.png"
+    }],
+    ['link', {
+      rel: "manifest",
+      href: "/"
+    }],
+    ['link', {
+      rel: "mask-icon",
+      href: "favicon.png",
+      color: "#3a0839"
+    }],
+    ['link', {
+      rel: "shortcut icon",
+      href: "favicon.png"
+    }],
+    ['meta', {
+      name: "msapplication-TileColor",
+      content: "#3a0839"
+    }],
+    ['meta', {
+      name: "msapplication-config",
+      content: "favicon.png"
+    }],
+    ['meta', {
+      name: "theme-color",
+      content: "#ffffff"
+    }],
   ],
-  
+
   plugins: [
+    googleAnalyticsPlugin({
+      id: 'G-58L8XFVVZ8',
+    }),
     searchPlugin({
       // options
     }),
-    gitPlugin({
-        // options
-      }),
-    googleAnalyticsPlugin({
-      // options
-      id: 'G-58L8XFVVZ8',
-    }),
   ],
-    
-    
-  
-    theme: defaultTheme({
-     // public file path
-      logo: 'webclip.png',
-      contributors: false,
-      docsRepo: 'https://github.com/connorwforsyth/docs.connorforsyth.co',
-      docsDir: 'docs',
-      docsBranch: 'master',
-      editLinkText: 'Edit this page',
-      navbar: [
-        {text: 'Portfolio', link: 'https://connorforsyth.co',},
-        {text: 'Knowledge', link: '/knowledge/',},
+
+  theme: defaultTheme({
+    // public file path
+    logo: 'webclip.png',
+    contributors: false,
+    docsRepo: 'https://github.com/connorwforsyth/docs.connorforsyth.co',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinkText: 'Edit this page',
+    navbar: [{
+        text: 'Portfolio',
+        link: 'https://connorforsyth.co',
+      },
+      {
+        text: 'Knowledge',
+        link: '/knowledge/',
+      },
     ],
     sidebar: {
-      '/knowledge/': [
-        {
-          title: 'Latin Dance',
-          collapsable: false,
-          children: [
-            '/Knowledge/latin-dance/cuban-salsa/rueda-de-casino'
-          ],
-        }
-        ]
+      '/knowledge/': [{
+        title: 'Latin Dance',
+        collapsable: false,
+        children: [
+          '/Knowledge/latin-dance/cuban-salsa/rueda-de-casino'
+        ],
+      }]
     }
   }),
 
-})
+}
